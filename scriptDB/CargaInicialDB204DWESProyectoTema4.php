4483b1b7583f816945e25ebbe9b6887a460027c4
Rebeca Sanchez <rebeca.sanper.1@educa.jcyl.es>
2023-12-01 09:55:55 +0100

    cambios en los scripts: "db.tabla" en vez de "use db"

--- scriptDB/CargaInicialDB204DWESProyectoTema4.php
+++ scriptDB/CargaInicialDB204DWESProyectoTema4.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * @author Rebeca Sánchez Pérez
- * @version 1.1
- * @since 29/11/2023
+ * @version 1.2
+ * @since 01/12/2023
  */
 
 // DECLARACION E INICIALIZACION DE VARIABLES
@@ -30,25 +30,8 @@ try {
     $consulta1 = $miDB->prepare($sql1);
     // Se ejecuta la consulta
     $consulta1->execute();
-    
-//    // Se crea una tabla para imprimir las tuplas
-//    echo('<div class="ejercicio"><h2>Con prepare() y execute():</h2><table class="ej16"><tr><th>Codigo</th><th>Descripcion</th><th>Fecha de alta</th><th>Volumen</th><th>Fecha de baja</th></tr>');
-//
-//    // Se recorre cada fila, es decir, cada departamento
-//    while ($oDepartamento = $consulta->fetchObject()) {// TAMBIEN SE PUEDE REALIZAR CON fetch(PDO::FETCH_OBJ)
-//        echo('<tr>');
-//        echo('<td>' . $oDepartamento->T02_CodDepartamento . '</td>');
-//        echo('<td>' . $oDepartamento->T02_DescDepartamento . '</td>');
-//        echo('<td>' . $oDepartamento->T02_FechaCreacionDepartamento . '</td>');
-//        echo('<td>' . $oDepartamento->T02_VolumenDeNegocio . '</td>');
-//        echo('<td>' . $oDepartamento->T02_FechaBajaDepartamento . '</td>');
-//        echo('</tr>');
-//    }
-//    echo('</table>');
-//    echo('</div><div class="ejercicio">');
-//    // Se muestra por pantalla el numero de tuplas  de la tabla departamentos
-//    echo('Numero total de registros: <b>' . $count . '</b>');
-//    echo('</div>');
+   
+    // Se muestra el mensaje de exito
     echo('Se han insertado los datos a la tabla "T02_Departamento" correctamente ✅');
 } catch (PDOException $exception) {
     // Si aparecen errores, se muestra por pantalla el error
--- scriptDB/CreaDB204DWESProyectoTema4.php
+++ scriptDB/CreaDB204DWESProyectoTema4.php
@@ -1,8 +1,8 @@
 <?php
 /*
  * @author Rebeca Sánchez Pérez
- * @version 1.1
- * @since 29/11/2023
+ * @version 1.2
+ * @since 01/12/2023
  */
 
 // DECLARACION E INICIALIZACION DE VARIABLES
@@ -17,13 +17,9 @@ try {
     // Se instancia un objeto tipo PDO que establece la conexion a la base de datos con el usuario especificado
     $miDB = new PDO('mysql:host=' . IPMYSQL . '; dbname=' . NOMBREDB, USUARIO, PASSWORD);
     
-    // Consulta de uso de la base de datos
-    $sql1 = <<< SQL
-        use dbs12302420;
-    SQL;
     // Consulta de creacion de la tabla departamento
-    $sql2 = <<< SQL
-        create table if not exists T02_Departamento(
+    $sql1 = <<< SQL
+        create table if not exists dbs12302420.T02_Departamento(
             T02_CodDepartamento varchar(3) primary key,
             T02_DescDepartamento varchar(255),
             T02_FechaCreacionDepartamento datetime,
@@ -33,29 +29,10 @@ try {
     
     // Se preparan las consultas
     $consulta1 = $miDB->prepare($sql1);
-    $consulta2 = $miDB->prepare($sql2);
     // Se ejecutan las consultas
     $consulta1->execute();
-    $consulta2->execute();
     
-//    // Se crea una tabla para imprimir las tuplas
-//    echo('<div class="ejercicio"><h2>Con prepare() y execute():</h2><table class="ej16"><tr><th>Codigo</th><th>Descripcion</th><th>Fecha de alta</th><th>Volumen</th><th>Fecha de baja</th></tr>');
-//
-//    // Se recorre cada fila, es decir, cada departamento
-//    while ($oDepartamento = $consulta->fetchObject()) {// TAMBIEN SE PUEDE REALIZAR CON fetch(PDO::FETCH_OBJ)
-//        echo('<tr>');
-//        echo('<td>' . $oDepartamento->T02_CodDepartamento . '</td>');
-//        echo('<td>' . $oDepartamento->T02_DescDepartamento . '</td>');
-//        echo('<td>' . $oDepartamento->T02_FechaCreacionDepartamento . '</td>');
-//        echo('<td>' . $oDepartamento->T02_VolumenDeNegocio . '</td>');
-//        echo('<td>' . $oDepartamento->T02_FechaBajaDepartamento . '</td>');
-//        echo('</tr>');
-//    }
-//    echo('</table>');
-//    echo('</div><div class="ejercicio">');
-//    // Se muestra por pantalla el numero de tuplas  de la tabla departamentos
-//    echo('Numero total de registros: <b>' . $count . '</b>');
-//    echo('</div>');
+    // Se muestra el mensaje de exito
     echo('Se ha creado la tabla "T02_Departamento" correctamente ✅');
 } catch (PDOException $exception) {
     // Si aparecen errores, se muestra por pantalla el error
